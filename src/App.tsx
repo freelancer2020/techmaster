ba4fcca5f5497ed90ea8b4ba99e95b08726d0f5a
Mostafa Mohamed <fantaenta21@gmail.com>
2021-09-03 22:48:43 +0200

    lunch gh

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FrontPage from "./components/Front";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <FrontPage />
+    </div>
+  );
+}
+
+export default App;
